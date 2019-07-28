36f57d090742057a42e439c8ac6dbaee76d9f9bb
maviek <mavioko7@gmail.com>
2019-07-28 22:34:06 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,16 @@
+mod app;
+mod lexer;
+mod parser;
+
+use std::fs::read_to_string;
+
 fn main() {
+    let args = app::Args::parse();
+    let src = match read_to_string(args.file) {
+        Err(e) => panic!("{}", e),
+        Ok(s) => s,
+    };
+
+    let tokens = lexer::lex(src);
+    parser::parse(tokens);
 }
