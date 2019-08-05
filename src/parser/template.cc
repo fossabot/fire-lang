c1e7adcd9acc403133c601eef8e448e23725d161
maviek <maviek@maviek.me>
2019-08-05 20:40:18 +0300

    Create template.cc

---
+++ src/parser/template.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdint>
+#include <string>
+
+#define __fire_stdout std::cout
+#define __fire_stdin std::cin
+
+typedef int8_t __fire_i8;
+typedef int16_t __fire_i16;
+typedef int32_t __fire_i32;
+typedef int64_t __fire_i64;
+typedef int8_t __fire_u8;
+typedef int16_t __fire_u16;
+typedef int32_t __fire_u32;
+typedef int64_t __fire_u64;
+typedef std::string __fire_string;
