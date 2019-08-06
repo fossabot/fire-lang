57e90a0f5c508345f8b7922300d008893c21fcc5
maviek <maviek@maviek.me>
2019-08-06 15:41:57 +0300

    Add vec and map

--- src/parser/template.cc
+++ src/parser/template.cc
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <cstdint>
 #include <string>
+#include <vector>
+#include <map>
 
+typedef float __fire_f32;
+typedef double __fire_f64;
 typedef int8_t __fire_i8;
 typedef int16_t __fire_i16;
 typedef int32_t __fire_i32;
@@ -11,6 +15,8 @@ typedef int16_t __fire_u16;
 typedef int32_t __fire_u32;
 typedef int64_t __fire_u64;
 typedef std::string __fire_string;
+#define __fire_vec std::vector
+#define __fire_map std::map
 
 template<typename T> void __fire_print(T t);
 template<typename T, typename... A> void __fire_print(T t, A... a);
